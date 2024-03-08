6b70d82e3dee646b9dd16f74344bf37ee70afbda
pwrignall <pwrignall@users.noreply.github.com>
2024-03-08 23:42:01 +0000

    Replace blog with landing page

--- _configs/notice.shortcode.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (data, noticeType, markdownLibrary) {
-  if (!noticeType) {
-    noticeType = "";
-  }
-  let noticeMarkup = markdownLibrary.renderInline(data);
-  return `<div class="notice ${noticeType}">${noticeMarkup}</div>`;
-}
-
